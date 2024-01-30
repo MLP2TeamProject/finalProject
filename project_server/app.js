57dfa30d690c0823c1a1d7f94123658138b06c14
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-30 13:59:50 +0900

    fix/bidding

--- project_server/app.js
+++ project_server/app.js
@@ -1,51 +1,74 @@
 //초기 설정
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const cors = require('cors')
-const nunjucks = require('nunjucks')
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
+const nunjucks = require("nunjucks");
+const cookieParser = require("cookie-parser"); //추가 npm 다운 받아야함
+const session = require("express-session"); //추가 npm 다운 받아야함
 
 // 프로젝트 루트에 .env 파일을 이용하겠다. .env를 다른폴더에서 사용하려면 config(매개변수)에 지정
-require('dotenv').config()
+require("dotenv").config();
 
-const homeRouter = require('./home/homeRouter')
+const homeRouter = require("./home/homeRouter");
+const productRouter = require("./product/productRouter");
 
-const app = express()
+const app = express();
 
 // nunjucks 등록 및 설정
-app.set('view engine', 'html')
-nunjucks.configure('common/views', {
-    express: app,
-    watch: true
-})
+app.set("view engine", "html");
+nunjucks.configure("common/views", {
+  express: app,
+  watch: true,
+});
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentiials: true,
+  })
+); //여기 추가 사진 요청 허용
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser("secret@1234"));
+app.use(
+  session({
+    secret: "secret@1234",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+    },
+  })
+);
+//여기 추가
+//쿠키 파서로 쿠키 파싱, 클라이언트로부터 오는 쿠키 데이터 읽음, 시크릿 키로 변조 방지
+//express-session 미들웨어로 세션 관리
 
 // 클라이언트 요청 데이터, 응답 데이터를 위해서 등록
-app.use(express.json())
-app.use(express.urlencoded({extended: true})) //http요청의 body parser(즉 form post요청 => request body에 인코딩된 데이터를 해석하여 req.body에 넣음)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //http요청의 body parser(즉 form post요청 => request body에 인코딩된 데이터를 해석하여 req.body에 넣음)
 
 // 개발자가 각 파일로 분리시킨 라우터 등록
-app.use('/', homeRouter)
+app.use("/", homeRouter);
+app.use("/products", productRouter);
 
 // 위에서 안걸린 요청은 404로 처리
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
-    error.status = 404
-    // 에러발생, 아래의 미들웨어가 처리함
-    next(error)
-})
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  // 에러발생, 아래의 미들웨어가 처리함
+  next(error);
+});
 
 // error handler middleware
 app.use((err, req, res, next) => {
-    res.locals.message = err.message
-    res.locals.error = process.env.NODE_ENV != 'production' ? err : {}
-    res.status(err.status || 500)
-    res.render('error') //error.html
-})
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV != "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error"); //error.html
+});
 
 app.listen(8000, () => {
-    console.log('8000번 포트에서 대기중...')
-})
+  console.log("8000번 포트에서 대기중...");
+});
--- project_server/product/productRouter.js
+++ project_server/product/productRouter.js
@@ -4,14 +4,6 @@ const multer = require("multer");
 const path = require("path");
 const router = express.Router();
 
-// router.get("/productList", function (req, res, next) {
-//   console.log("상품 메인 불러오기");
-//   productDAO.productList((resp) => {
-//     res.json(resp);
-//   });
-// });
-// 화면메인 부분 -준영님
-
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, done) {
@@ -25,10 +17,13 @@ const upload = multer({
   limits: { fileSize: 5 * 1024 * 1024 },
 });
 
-router.post("/upload", async (req, res, next) => {
-  const biddingUpload = upload.single("file1");
+router.post("/bidding/insert", async (req, res, next) => {
+  console.log("0000000");
 
-  biddingUpload(req, res, function (err) {
+  //파일 업로드 처리하고.. 이 라인에서 에러가 발생하지 않으면 파일 업로드 성공
+  const a1 = upload.single("file1");
+
+  a1(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       console.log(err);
       res.json({ status: 500, message: "error" });
@@ -36,37 +31,18 @@ router.post("/upload", async (req, res, next) => {
       console.log(err);
       res.json({ status: 500, message: "error" });
     } else {
+      //에러가 없다면.. 나머지 데이터를 받는다..
+      console.log("upload router....");
       const data = req.body;
-      // 파일 업로드 성공시
-      console.log("00", data.inputData);
-      console.log("11", req.file.filename);
-      auctionDAO.bidding(data.inputData, req.file.filename, (resp) => {
+      const obj = JSON.parse(data.sendData);
+
+      console.log("sendData", obj);
+      productDAO.bidding(obj, (resp) => {
         res.json(resp);
       });
     }
   });
 });
-
-//upload 엔드포인트에 대한 POST 요청 처리
-// router.post("/upload", (req, res, next) => {
-//   const pictureUpload = upload.single("file1"); //여기 업로드할 파일의 필드이름= file1을 front bidding이랑 맞춤
-//   pictureUpload(req, res, function (err) {
-//     if (err instanceof multer.MulterError) {
-//       console.log(err);
-//       res.json({ status: 500, message: "error" });
-//     } else if (err) {
-//       console.log(err);
-//       res.json({ status: 500, message: "error" });
-//     } else {
-//       console.log("파일 업로드 완료!");
-//       const data = req.body;
-//       console.log("title", data.title);
-//       console.log("file", req.file.filename);
-//       //여기에
-//       res.json({ status: 200, message: "OK", data: req.file.filename });
-//     }
-//   });
-// });
 
 router.get("/detail/:id", function (req, res, next) {
   console.log("디테일 불러오기");
@@ -76,14 +52,5 @@ router.get("/detail/:id", function (req, res, next) {
     res.json(resp);
   });
 });
-
-// router.post("/update", function (req, res, next) {
-//   console.log("게시글 수정하기");
-//   const data = req.body;
-//   productDAO.update(data, (resp) => {
-//     res.json(resp);
-//   });
-// });
-//이 부분은 게시글 작성자가 수정할 때의 부분이라...-준영님
 
 module.exports = router;
