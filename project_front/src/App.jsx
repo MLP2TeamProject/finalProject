b65b881bd026abdc14bcc5d2c31288776ed16d1d
hyuni <sareng00@naver.com>
2024-01-31 13:16:19 +0900

    error

--- project_front/src/App.jsx
+++ project_front/src/App.jsx
@@ -1,22 +1,35 @@
-import { Routes, Route } from 'react-router-dom'
-
-import Header from './home/component/Header'
-
-import BoardMain from './board/BoardMain'
-import MypageMain from './mypage/MypageMain'
+import { Routes, Route } from "react-router-dom";
+import Header from "./home/component/Header";
+import Footer from "./home/component/Footer";
+import HomeMain from "./home/HomeMain";
+import MypageMain from "./mypage/MypageMain";
+import UserMain from "./user/UserMain";
+import BoardMain from "./board/BoardMain";
+import ProductMain from "./product/ProductMain";
 
 const App = () => {
   return (
-    <div>
-      {/* Header와 footer는 공통적으로 작용 */}
-      <Header/>
+    <>
+      <Header />
       <Routes>
-        <Route path='/board/*' element={<BoardMain />} />
-        <Route path='/mypage/*' element={<MypageMain />} />
+        <Route path="/" element={<HomeMain />} />
+        <Route path="/board/*" element={<BoardMain />} />
+        <Route path="/user/*" element={<UserMain />} />
+        <Route path="/mypage/*" element={<MypageMain />} />
+        <Route path="/products/*" element={<ProductMain />} />
+        <Route
+          path="*"
+          element={
+            <>
+              <hr />
+              <h3>Page Not Found</h3>
+            </>
+          }
+        />
       </Routes>
-      {/* <Footer /> */}
-    </div>
-  )
-}
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
