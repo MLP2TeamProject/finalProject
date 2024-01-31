0cf3cbeca1b61163f7cd010f38947307dffffa26
eykang <delikeyki1016@gmail.com>
2024-01-31 09:51:49 +0900

    ProductMain 컴포넌트 연결 삭제

--- project_front/src/App.jsx
+++ project_front/src/App.jsx
@@ -4,7 +4,6 @@ import Footer from './home/component/Footer'
 import HomeMain from './home/HomeMain'
 import MypageMain from './mypage/MypageMain'
 import UserMain from './user/UserMain'
-import ProductList from './product/ProductMain'
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route path='/' element={<HomeMain />} />
           <Route path='/user/*' element={<UserMain />} />
           <Route path='/mypage/*' element={<MypageMain />} />
-          <Route path='/product' element={<ProductList />} />
           <Route path='*' element={<><hr/><h3>Page Not Found</h3></>} />
         </Routes>
       <Footer /> 
