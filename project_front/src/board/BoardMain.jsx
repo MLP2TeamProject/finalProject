c1cadc28a462edb9a50fcbf641065c00476b8bc6
DY <rbrbdk12@gmail.com>
2024-02-13 13:10:40 +0900

    refactor: board main 수정

--- project_front/src/board/BoardMain.jsx
+++ project_front/src/board/BoardMain.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import FaqBoardList from "./component/FaqBoardList";
 
 import './board.css'
@@ -12,22 +13,26 @@ import NoticeBoardList from "./component/NoticeBoardList";
 
 const BoardMain = () => {
     return (
-        <div>
-            {/* <h2>test - Board Main</h2> */}
+        <>
+            <div>
+                {/* <h2>test - Board Main</h2> */}
+            </div>
+
             <Routes>
                 {/* noticeboard */}
                 <Route path="/noticelist" element={<NoticeBoardList />} />
-                
+
                 {/* 상세페이지, 삭제까지 */}
                 <Route path="/noticedetail/:id" element={<NoticeBoardDetail />} />
                 <Route path="/noticeinsert" element={<NoticeBoardInsert />} />
                 <Route path="/noticeupdate/:id" element={<NoticeBoardUpdate />} />
-                
+
                 {/* faqboard */}
                 <Route path="/faqlist" element={<FaqBoardList />} />
                 {/* FaqBoardDetail에서 상세페이지, 삭제까지 */}
             </Routes>
-        </div>
+        </>
+
     );
 };
 export default BoardMain;
